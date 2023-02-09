15037de3555728cd9f42dc11ddba1f6c10060d37
Atharva Narkhede <106006803+atharva-narkhede@users.noreply.github.com>
2023-02-09 21:08:04 +0530

    Add files via upload

---
+++ Control Statements/pattern6.java
@@ -0,0 +1,22 @@
+package SRM;
+import java.util.*;
+public class pattern6 {
+	public static void main(String[] args) {
+        int rows;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter number of rows: ");
+        rows = scan.nextInt();
+        for (int i = 1; i <= rows; i++) {
+
+            System.out.print(" ".repeat(rows - i));
+            for (int k = i; k >= 1; k--) {
+                System.out.print(k);
+            }
+            for (int j = 2; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+
+    }
+}
